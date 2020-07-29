a4a68e33f25de1f6e082a0e5c0bae45f877f245c
Xzonn <Xzonn2000@gmail.com>
2020-07-29 20:40:01 +0800

    2020-07-29：Hashed

--- includes/GitHubImage.php
+++ includes/GitHubImage.php
@@ -1,10 +1,15 @@
 <?php
 class GitHubImage {
     public static function onLinkerMakeMediaLinkFile ( $title, $file, &$html, &$attribs, &$ret ) {
-        global $wgXzGhiUsername, $wgXzGhiRepo;
+        global $wgXzGhiUsername, $wgXzGhiRepo, $wgXzHashed, $wgHashedUploadDirectory;
+		$subPath = '';
+		if ( $wgXzHashed and $wgHashedUploadDirectory ) {
+			$md5 = md5(str_replace('_', ' ', $file->getName()));
+			$subPath = substr( $md5, 0, 1 ) . '/' . substr( $md5, 0, 2 ) . '/';
+		}
         $attribs[ 'rel' ] = 'nofollow';
         $attribs[ 'class' ] = 'external text';
-        $attribs[ 'href' ] = "https://github.com/$wgXzGhiUsername/$wgXzGhiRepo/blob/master/" . $title->mUrlform;
+        $attribs[ 'href' ] = "https://github.com/$wgXzGhiUsername/$wgXzGhiRepo/blob/master/$subPath" . $title->mUrlform;
         return true;
     }
 }
--- includes/XzImageHandler.php
+++ includes/XzImageHandler.php
@@ -1,21 +1,30 @@
 <?php
 class XzImageHandler extends BitmapHandler {
 	function doTransform( $image, $dstPath, $dstUrl, $params, $flags = 0 ) {
-        global $wgXzGhiUsername, $wgXzGhiRepo;
 		if ( !$this->normaliseParams( $image, $params ) ) {
 			return new TransformParameterError( $params );
-        }
-        return new ThumbnailImage( $image, "https://cdn.jsdelivr.net/gh/$wgXzGhiUsername/$wgXzGhiRepo/" . $image->getName(), null, $params );
+		}
+        global $wgXzGhiUsername, $wgXzGhiRepo, $wgXzHashed, $wgHashedUploadDirectory;
+		$subPath = '';
+		if ( $wgXzHashed and $wgHashedUploadDirectory ) {
+			$md5 = md5(str_replace('_', ' ', $image->getName()));
+			$subPath = substr( $md5, 0, 1 ) . '/' . substr( $md5, 0, 2 ) . '/';
+		}
+        return new ThumbnailImage( $image, "https://cdn.jsdelivr.net/gh/$wgXzGhiUsername/$wgXzGhiRepo/$subPath" . $image->getName(), null, $params );
 	}
 }
 class XzSvgHandler extends SvgHandler {
 	public function doTransform( $image, $dstPath, $dstUrl, $params, $flags = 0 ) {
-        global $wgXzGhiUsername, $wgXzGhiRepo;
 		if ( !$this->normaliseParams( $image, $params ) ) {
 			return new TransformParameterError( $params );
 		}
-
-		return new SvgImage( $image, "https://cdn.jsdelivr.net/gh/$wgXzGhiUsername/$wgXzGhiRepo/" . $image->getName(), $params['width'], $params['height'], $image->getPath() );
+        global $wgXzGhiUsername, $wgXzGhiRepo, $wgXzHashed, $wgHashedUploadDirectory;
+		$subPath = '';
+		if ( $wgXzHashed and $wgHashedUploadDirectory ) {
+			$md5 = md5(str_replace('_', ' ', $image->getName()));
+			$subPath = substr( $md5, 0, 1 ) . '/' . substr( $md5, 0, 2 ) . '/';
+		}
+		return new SvgImage( $image, "https://cdn.jsdelivr.net/gh/$wgXzGhiUsername/$wgXzGhiRepo/$subPath" . $image->getName(), $params['width'], $params['height'], $image->getPath() );
 	}
 
 	public function getThumbType( $ext, $mime, $params = null ) {
